cb23eceeacb97eead803995f1e92dd956a57810c
Felipe Caruso <101845224+felipecarusoo@users.noreply.github.com>
2025-05-14 14:31:18 +0000

    .

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let numeroSenha = 12
+const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz'
+const numeros = '0123456789'
+const simbolos = '@!/%$*();?{}'
+const numeroSenha = document.querySelector('.paremetro-senha__texto')
+const botoes = document.querySelectorAll('.parametro-senha__botao')
+const campoSenha = document.querySelector('#campo-senha')
+const checkbox = document.querySelectorAll('.checkbox')
+const forcaSenha = document.querySelector ('.forca')
